a39a7bcc6ca6a1696cc9d822d5fce89193c46090
Ilya <ilyaagent765@gmail.com>
2017-03-12 16:03:44 +0300

    Initial commit

---
+++ src/ChessDesk.java
@@ -0,0 +1,14 @@
+/**
+ * Created by USER on 12.03.2017.
+ */
+public class ChessDesk {
+    public static void main(String[] args) {
+        for(int i=0;i<8; i++){
+            if(i%2==0){
+                System.out.print(1);
+            }else{
+                System.out.print(0);
+            }
+        }
+    }
+}
